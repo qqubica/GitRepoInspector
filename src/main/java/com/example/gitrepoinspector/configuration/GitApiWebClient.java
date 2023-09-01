94d4f5e64bb3bd3bc4a086066740ea27b11c683b
Kuba Wudarski <kuba@wudarscy.pl>
2023-09-01 15:27:30 +0200

    webClient

---
+++ src/main/java/com/example/gitrepoinspector/configuration/GitApiWebClient.java
@@ -0,0 +1,27 @@
+package com.example.gitrepoinspector.configuration;
+
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class GitApiWebClient {
+    String baseUrl = "https://api.github.com/";
+    @Value("${git.token}")
+    String token;
+
+    @Bean
+    public WebClient webClient() {
+        WebClient.Builder webClient= WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader("X-GitHub-Api-Version", "2022-11-28");
+        if (!token.trim().equals("")) {
+            webClient.defaultHeader("Authorization", "Bearer " + token);
+        }
+        return webClient.build();
+    }
+
+}
