065130ce3ad5de8bbb0c47e812ea689d510dad53
Kuba Wudarski <kuba@wudarscy.pl>
2023-09-01 15:31:38 +0200

    import optimalization

--- src/main/java/com/example/gitrepoinspector/configuration/GitApiWebClient.java
+++ src/main/java/com/example/gitrepoinspector/configuration/GitApiWebClient.java
@@ -7,18 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class GitApiWebClient {
     String baseUrl = "https://api.github.com/";
     @Value("${git.token}")
-    String token;
+    String token = "";
 
     @Bean
     public WebClient webClient() {
-        WebClient.Builder webClient= WebClient.builder()
+        WebClient.Builder webClient = WebClient.builder()
                 .baseUrl(baseUrl)
                 .defaultHeader("X-GitHub-Api-Version", "2022-11-28");
-        if (!token.trim().equals("")) {
+        if (!token.isEmpty()) {
             webClient.defaultHeader("Authorization", "Bearer " + token);
         }
         return webClient.build();
--- src/main/java/com/example/gitrepoinspector/exceptionHandler/MyErrorHandlers.java
+++ src/main/java/com/example/gitrepoinspector/exceptionHandler/MyErrorHandlers.java
@@ -7,20 +7,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
 @ControllerAdvice
 public class MyErrorHandlers {
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-    protected ResponseEntity handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException ex, HttpServletRequest request) {
+    protected ResponseEntity<ApiError> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException ex, HttpServletRequest request) {
         String errorMessage = "accept: " + request.getHeader("accept") + " header is not supported";
         return new ResponseEntity<>(new ApiError(ex.getStatusCode().value(), errorMessage), ex.getStatusCode());
     }
 
     @ExceptionHandler({ResponseStatusException.class})
-    public ResponseEntity<?> handleResponseStatusException(ResponseStatusException ex) {
+    public ResponseEntity<ApiError> handleResponseStatusException(ResponseStatusException ex) {
         return new ResponseEntity<>(new ApiError(ex.getStatusCode().value(), ex.getReason()), ex.getStatusCode());
     }
 }
